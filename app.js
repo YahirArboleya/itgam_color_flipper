bdd3b9aea2e5d9699b884c82b5d6d9d9b3a1f449
Yahir <yahir2_telas@outlook.com>
2024-05-06 08:38:13 -0600

    Color simple

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// get the button element
+const btn = document.getElementById("btn");
+// get the color element
+const color = document.querySelector(".color");
+
+// add event listener in the button
+btn.addEventListener("click", () => {
+    // get a random number entre 0 y 3
+    const randomNumber = getRandomNumber();
+    // set the background color
+    const colorPicked= colors[randomNumber];
+    // color del body
+    document.body.style.backgroundColor = colorPicked;
+    // color del texto
+    color.textContent = colorPicked;
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
